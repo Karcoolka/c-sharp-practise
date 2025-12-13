8c0e5a480a53a45a6471922adf4bdb0af4cae177
Adela Simkova <adela@sherpadevs.com>
2025-12-13 11:51:53 +0100

    first beginner exercise

---
+++ CSharp_practise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Exercise.exercises;
+
+class Program
+{
+
+    static void Main()
+    {
+        // Beginner
+        string input = "hello2";
+        string reversed = Beginner.ReverseString(input); 
+        Console.WriteLine(reversed);            
+    }
+}
---
+++ CSharp_practise/exercises/Beginner.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercise.exercises;
+
+public class Beginner
+{
+    // Function to reverse a string
+    public static string ReverseString(string input)
+    {
+        char[] charArray = input.ToCharArray(); 
+        Array.Reverse(charArray);               
+        return new string(charArray);           
+    }
+}
